3dff680761c511f2f3e9e9f0b9c3272f6b0d124a
Bum_Bam <timurrozmetow17@gmail.com>
2024-02-02 09:09:54 +0500

    first commit

---
+++ scriptParola.js
@@ -0,0 +1,10 @@
+let login = prompt("Adynyz:", "");
+let pass = prompt("Kodynyz:", "");
+
+let Ady = 'Amangul';
+let kod = '865832616';
+if (pass == null || login == null) window.location="kod.html";
+else if (pass === kod && login === Ady)
+    window.location="main.html";
+else window.location="kod.html";
+
