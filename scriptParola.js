cf93f21c34aa15d4683d5f19aafb88ac157a2b01
Bum_Bam <53340829+timurrozmetow@users.noreply.github.com>
2024-08-30 20:41:58 +0500

    Update scriptParola.js

--- scriptParola.js
+++ scriptParola.js
@@ -1,7 +1,7 @@
 let login = prompt("Adynyz:", "");
 let pass = prompt("Kodynyz:", "");
 
-let Ady = 'Amangul';
+let Ady = 'Amangul12';
 let kod = '865832616';
 if (pass == null || login == null) window.location="kod.html";
 else if (pass === kod && login === Ady)
